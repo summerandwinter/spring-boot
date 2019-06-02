be1fdc67711c334f2a6c3c4be7a7912e9ef01fe3
刘毅 <yi.liu@bmsoft.com.cn>
2019-06-02 20:24:37 +0800

    增加自定义 metrics

---
+++ spring-boot-actuator/src/main/java/com/summer/springboot/actuator/config/MetricsConfig.java
@@ -0,0 +1,18 @@
+package com.summer.springboot.actuator.config;
+
+import com.summer.springboot.actuator.metrics.DemoMetrics;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author yi.liu@bmsoft.com.cn
+ * @date 2019/6/2
+ */
+@Configuration
+public class MetricsConfig {
+
+  @Bean
+  public DemoMetrics demoMetrics() {
+    return new DemoMetrics();
+  }
+}
---
+++ spring-boot-actuator/src/main/java/com/summer/springboot/actuator/metrics/DemoMetrics.java
@@ -0,0 +1,29 @@
+package com.summer.springboot.actuator.metrics;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.binder.MeterBinder;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author yi.liu@bmsoft.com.cn
+ * @date 2019/6/2
+ */
+public class DemoMetrics  implements MeterBinder {
+  private AtomicInteger count = new AtomicInteger(0);
+  @Override
+  public void bindTo(MeterRegistry meterRegistry) {
+    Gauge.builder("demo.gauge", count, AtomicInteger::get)
+        .tags("host", "localhost")
+        .description("demo of custom metrics")
+        .baseUnit("beans")
+        .register(meterRegistry);
+    Counter counter = Counter.builder("demo.counter")
+        .baseUnit("beans")
+        .description("a description of what this counter does")
+        .tags("region", "test")
+        .register(meterRegistry);
+    counter.increment();
+  }
+}
