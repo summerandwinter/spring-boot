39ca11f1944959e1ffa1747e6c2f0851a0fdff02
yi.liu <ly1989124423@163.com>
2019-07-05 18:01:07 +0800

    增加 OAuth2 资源服务

---
+++ spring-boot-oauth2/spring-boot-oauth2-resource-server/src/main/java/io/summer/springboot/oauth2/resources/AuthResourceServerApplication.java
@@ -0,0 +1,16 @@
+package io.summer.springboot.oauth2.resources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author summerandwinter
+ * @date 2019/7/2
+ */
+@SpringBootApplication
+public class AuthResourceServerApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(AuthResourceServerApplication.class, args);
+  }
+
+}
